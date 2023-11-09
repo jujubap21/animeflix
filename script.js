acadb5e1466d45fbd0719de8693dc74a7bb12d04
Júlia Pfister <148919434+jujubap21@users.noreply.github.com>
2023-11-09 17:10:17 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+let listaAnimes = [
+  "https://www.intoxianime.com/wp-content/uploads/2023/06/kv-724x1024-1.jpg",
+
+  "https://sportshub.cbsistatic.com/i/2023/08/24/c978d7c1-b2ab-4965-b712-f1b5bd50711e/jujutsu-kaisen.jpg?auto=webp&width=863&height=1200&crop=0.719:1,smart",
+
+  "https://m.media-amazon.com/images/I/81soG-Z-UPL._AC_UF894,1000_QL80_.jpg",
+
+  "https://anayaoionline.files.wordpress.com/2018/10/wp-1539486550454.jpeg?w=640",
+
+  "https://johto.legiaodosherois.com.br/wp-content/uploads/2020/12/legiao_BgWb2m1i4aEs.jpg",
+
+  "https://images.justwatch.com/poster/302451482/s718/season-4.jpg",
+
+  "https://br.web.img2.acsta.net/pictures/22/04/18/22/43/1487268.jpg",
+
+  "https://m.media-amazon.com/images/M/MV5BODFmYTUwYzMtM2M2My00NGExLWIzMDctYmRjNTNhZDc4MGI2XkEyXkFqcGdeQXVyMTMzNDExODE5._V1_.jpg",
+
+  "https://m.media-amazon.com/images/I/71QS2yNxhoL._AC_UF1000,1000_QL80_.jpg",
+
+  "https://m.media-amazon.com/images/M/MV5BMmRhZDI4ODItYTAzMC00NWQ1LTk1MTctZWYyMDY3ZTU3NWFiXkEyXkFqcGdeQXVyMzgxODM4NjM@._V1_FMjpg_UX1000_.jpg",
+
+  "https://br.web.img2.acsta.net/pictures/21/05/10/23/27/0599462.jpg",
+
+  "https://upload.wikimedia.org/wikipedia/pt/d/dd/Re_Zero_poster.jpg",
+
+  "https://m.media-amazon.com/images/M/MV5BYzk4NDc5MTAtZGQzOC00Y2MyLThkMGEtOGRkMzliOGNlYjkwXkEyXkFqcGdeQXVyMTA5NzUzODM4._V1_.jpg",
+];
+
+listaAnimes.push(
+  "https://www.intoxianime.com/wp-content/uploads/2021/04/Ez_Sv5cUUAUXdDg-724x1024.jpg"
+);
+
+for (let i = 0; i < listaAnimes.length; i++) {
+  document.write("<img src=" + listaAnimes[i] + ">");
+}
